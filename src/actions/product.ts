9da8020647ee28dd3bebb7b98f14bdd17f3974c9
hon <as02700@qq.com>
2016-09-27 21:59:06 +0800

    添加 @ngrx/store @ngrx/effect 修改 product component 对接 @ngrx/store.

---
+++ src/actions/product.ts
@@ -0,0 +1,51 @@
+import { Action } from '@ngrx/store';
+
+var toActionType = ( desc ) => [ '[product]',' ',desc ].join('');
+var toActionCreator = ( type ) => ( payload:any ):Action => { return {type:type, payload:payload}; };
+
+
+export default class ProductActionCreator {
+
+    static CREATE = toActionType('create');
+    static CREATE_RESULT = toActionType('create_result');
+
+    static UPDATE = toActionType('update');
+    static UPDATE_RESULT = toActionType('update_result');
+
+    static REMOVE = toActionType('remove');
+    static REMOVE_RESULT = toActionType('remove_result');
+
+    static SEARCH = toActionType('search');
+    static SEARCH_RESULT = toActionType('search_result');
+
+    create;
+    create_result;
+
+    update;
+    update_result;
+
+    remove;
+    remove_result;
+
+    search;
+    search_result;
+
+    constructor() {
+        [
+            'create',
+            'update',
+            'remove',
+            'search'
+        ].map( function( x ) {
+            return [ x, [x, 'result'].join('_') ];
+        } ).reduce( function( l, r ) {
+            return l.concat( r );
+        }, [] ).forEach(
+            ( k ) => {
+                var t = this.constructor[ k.toUpperCase() ];
+                this[k] = toActionCreator( t );
+            }
+        );
+    }
+
+}
--- src/products/products.ts
+++ src/products/products.ts
@@ -3,18 +3,36 @@ import { Component } from '@angular/core';
 import { CrudService } from '../services/crud_service';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+import ProductActionCreator from '../actions/product';
+import Product from '../classes/product';
+
 @Component({
   selector: 'products',
   template: require('./products.pug'),
   providers: [CrudService],
 })
 export class ProductsComp {
-  products: MyApp.Product[];
-  api = this.crud.sub.product;
+  products;
 
   constructor (
     // public router: Router,
-    private crud: CrudService,
-  ) {}
+    private store: Store,
+    private productAction: ProductActionCreator
+  ) {
+    this.products = store.select('products');
+  }
+
+  ngOnInit() {
+    this.store.dispatch( this.productAction.search() );
+  }
+
+  create_product( evt ) {
+    var p = new Product();
+    p.id = -1;
+    p.name = 'Snazzy Widget';
+    p.price = 100;
+    this.store.dispatch( this.productAction.create(p) );
+  }
 
 }
